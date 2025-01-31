1d12ffc0d661bae36695e72757f1beda8254bc0e
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2025-01-31 17:54:45 +0100

    miami: evo x

--- lineage_miami.mk
+++ lineage_miami.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/motorola/miami/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+WITH_GMS := true
+BUILD_BCR := true
+TARGET_HAS_UDFPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_miami
